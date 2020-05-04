504a2c73c5d146071b64a729d27f88dcdcf657ef
sauravsb99 <sauravsb99@gmail.com>
2020-05-05 00:13:01 +0530

    Tetsting auth

---
+++ queastio/lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:queastio/screens/authenticate/authenticate.dart';
+import 'package:queastio/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Authenticate();
+  }
+}
