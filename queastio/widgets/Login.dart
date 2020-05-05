6e10daa8950c017f055fdbf6f7a543e9c07f2baa
sachin-in1 <saching1998@cet.ac.in>
2020-05-05 23:32:57 +0530

    widget folder added-textbox widget

---
+++ queastio/widgets/Login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final emailField = TextField(
+  decoration: InputDecoration(
+      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      hintText: "Email",
+      border:
+      OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+);
+
