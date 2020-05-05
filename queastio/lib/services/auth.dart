7212b6ff733556dd569717083e47df88d24b81d2
sauravsb99 <sauravsb99@gmail.com>
2020-05-05 16:06:16 +0530

    sign in anonym

---
+++ queastio/lib/services/auth.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:queastio/models/user.dart';
+
+class AuthService{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User _userFromFirebaseUser(FirebaseUser user){
+    return user != null ? User(uid: user.uid): null;
+  }
+
+  Future signInAnon() async{
+    try{
+
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+
+
+}
