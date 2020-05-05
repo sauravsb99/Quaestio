3aacd69ffeba6080cdc669e5086b1870c407d1d1
sachin-in1 <saching1998@cet.ac.in>
2020-05-06 00:51:58 +0530

    textboxes

--- queastio/lib/screens/authenticate/sign_in.dart
+++ queastio/lib/screens/authenticate/sign_in.dart
@@ -9,7 +9,47 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   AuthService _auth = AuthService();
   @override
+
   Widget build(BuildContext context) {
+
+    final emailField = TextField(
+      obscureText: false,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(11.0))),
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(11.0))),
+    ); //password
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+        child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () async{
+          dynamic result = await _auth.signInAnon();
+          if(result == null){
+            print('Eroor signing in');
+          }else{
+            print('Welcome ');
+            print(result.uid);
+          }
+         },
+          child: Text("Login",textAlign: TextAlign.center,style: TextStyle(
+          color: Colors.white, fontWeight: FontWeight.bold))
+        )
+    );
+
     return Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
@@ -17,22 +57,38 @@ class _SignInState extends State<SignIn> {
         elevation: 0.0,
         title: Text('Sign In to Quaestion'),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
-        child: RaisedButton(
-          child: Text('sign in anon'),
-          onPressed: () async{
-            dynamic result = await _auth.signInAnon();
-            if(result == null){
-              print('Eroor signing in');
-            }else{
-              print('Welcome ');
-              print(result.uid);  
-            }
-          },
-        ),
+
+      body: Container(padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+//          SizedBox(
+//            height: 155.0,
+//            child: Image.asset(
+//              "assets/logo.png",
+//              fit: BoxFit.contain,
+//            ),
+//          ),
+          SizedBox(height: 45.0),
+          emailField,
+          SizedBox(height: 25.0),
+          passwordField,
+//          SizedBox(
+//            height: 35.0,
+//          ),
+//
+//        loginButon,
+          SizedBox(
+              height: 15.0),
+          loginButton,
+
+        ],
+      ),
       ),
 
     );
   }
+
+//  loginButton({Text child, Future<Null> Function() onPressed}) {}
 }
--- queastio/widgets/Login.dart
+++ queastio/widgets/Login.dart
@@ -1,11 +1,37 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+//fast access of widgets
+
+
 
 final emailField = TextField(
   decoration: InputDecoration(
       contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
       hintText: "Email",
       border:
-      OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      OutlineInputBorder(borderRadius: BorderRadius.circular(11.0))),
 );
 
+final PasswordField = TextField(
+  obscureText: true,
+  decoration: InputDecoration(
+      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      hintText: "Password",
+      border:
+      OutlineInputBorder(borderRadius: BorderRadius.circular(11.0))),
+);
+
+final loginButon = Material(
+  elevation: 5.0,
+  borderRadius: BorderRadius.circular(30.0),
+  color: Color(0xff01A0C7),
+  child: MaterialButton(
+    padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+    onPressed: () {},
+    child: Text("Login",textAlign: TextAlign.center,style: TextStyle(
+        color: Colors.white, fontWeight: FontWeight.bold)),
+  ),
+);
