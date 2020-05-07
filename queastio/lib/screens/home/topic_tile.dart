59ad5c48eacfa443fb23472663f94f564c16e6ac
sachin-in1 <saching1998@cet.ac.in>
2020-05-07 21:50:30 +0530

    Merge branch 'sachin' of /home/sachin/Desktop/Queastio/Quaestio with conflicts.

--- queastio/lib/screens/home/topic_tile.dart
+++ queastio/lib/screens/home/topic_tile.dart
@@ -5,49 +5,29 @@ class TopicTile extends StatelessWidget {
   final Topic topic;
   TopicTile({this.topic});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//         child: Padding(
-//       padding: const EdgeInsets.all(8.0),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.start,
-//         children: <Widget>[
-//           CircleAvatar(
-//             radius: 40.0,
-//             backgroundImage: NetworkImage(topic.image),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(25.0),
-//             child: Text(
-//               topic.name,
-//               style: TextStyle(
-//                 fontSize: 20.0,
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     ));
-//   }
-// }
-
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8.0),
-      child: Card(
-        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            radius: 25.0,
-            // backgroundColor: Colors.brown[topic.],
+    return Card(
+        child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 40.0,
+            backgroundImage: NetworkImage(topic.image),
           ),
-          title: Text(topic.name),
-          // subtitle: Text('Takes ${brew.sugars} sugar(s)'),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Text(
+              topic.name,
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
