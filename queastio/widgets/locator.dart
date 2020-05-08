e82cd784a685b80d34b8bd59f976f5fc61a4d19b
sachin-in1 <saching1998@cet.ac.in>
2020-05-08 23:22:08 +0530

    uponly,download error, (bhaviyil email should be in database)

--- queastio/widgets/locator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:queastio/services/auth.dart';
-import 'package:queastio/screens/profileScreen/Usercontroller.dart';
-import 'package:get_it/get_it.dart';
-
-final locator =GetIt.instance;
-
-void setupServices{
-  locator.register
-}
