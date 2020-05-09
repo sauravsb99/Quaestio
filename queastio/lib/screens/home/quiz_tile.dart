64925342c7031d3185c0b3b09d85bd7e47de25a8
adarsh-av13 <dra4474@gmail.com>
2020-05-09 13:35:52 +0530

    quiz details sheet added

--- queastio/lib/screens/home/quiz_tile.dart
+++ queastio/lib/screens/home/quiz_tile.dart
@@ -9,12 +9,94 @@ class QuizTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showQuizDetails() {
+      print('dsfsdf');
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Divider(height: 20.0),
+                  Text(
+                    quiz.qName.toUpperCase(),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
+                      letterSpacing: 2.0,
+                      wordSpacing: 5.0,
+                    ),
+                  ),
+                  Divider(
+                    height: 30.0,
+                    thickness: 5.0,
+                    color: Colors.indigo,
+                    indent: 0,
+                  ),
+                  Text(
+                    'lorem idasfs sdsad fwr dfs asda wewe fdgv sdas dg asd dsvfds sfedet gddhrd sdfdsgs',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'No of questions: 10',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(height: 50.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(2.0),
+                        decoration: BoxDecoration(
+                          color: Colors.indigo[900],
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        child: Text(
+                          'Topic',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      RaisedButton(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 5.0),
+                        onPressed: () {},
+                        child: Text(
+                          'Start Quiz',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.grey[50],
+                          ),
+                        ),
+                        color: Colors.limeAccent[700],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
+          });
+    }
+
     return InkWell(
       splashColor: Colors.indigo[100],
       splashFactory: InkSplash.splashFactory,
       onTap: () {
         print(quiz.questions);
-        // Navigator.pushNamed(context, '/quizzes', arguments: quiz.qName);
+        _showQuizDetails();
       },
       child: Card(
           child: Padding(
@@ -25,7 +107,7 @@ class QuizTile extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(25.0),
               child: Text(
-                quiz.qName,
+                quiz.qName.toUpperCase(),
                 style: TextStyle(
                   fontSize: 20.0,
                   color: Colors.black,
