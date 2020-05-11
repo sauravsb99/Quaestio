05397e9390b69b559b8a6259f7344291e4c9cca7
adarsh-av13 <dra4474@gmail.com>
2020-05-12 02:58:31 +0530

    uname added to scores

--- queastio/lib/models/score.dart
+++ queastio/lib/models/score.dart
@@ -1,9 +1,10 @@
 class Score {
   final String quiz;
   final String qTopic;
+  final String uname;
   final String uid;
   final int score;
   final int total;
 
-  Score({this.quiz, this.qTopic, this.uid, this.score, this.total});
+  Score({this.quiz, this.qTopic, this.uid, this.uname, this.score, this.total});
 }
