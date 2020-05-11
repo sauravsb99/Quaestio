600d29639b938e7966bc6798d32c0b60c66c2509
adarsh-av13 <dra4474@gmail.com>
2020-05-11 22:21:50 +0530

    scores set

--- queastio/lib/models/score.dart
+++ queastio/lib/models/score.dart
@@ -1,8 +1,9 @@
 class Score {
   final String quiz;
+  final String qTopic;
   final String uid;
   final int score;
   final int total;
 
-  Score({this.quiz, this.uid, this.score, this.total});
+  Score({this.quiz, this.qTopic, this.uid, this.score, this.total});
 }
