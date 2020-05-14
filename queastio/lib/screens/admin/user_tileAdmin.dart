b2716cce09bee8a2144461fb11c3bd31f0c03a37
adarsh-av13 <dra4474@gmail.com>
2020-05-14 16:04:45 +0530

    error fixes

--- queastio/lib/screens/admin/user_tileAdmin.dart
+++ queastio/lib/screens/admin/user_tileAdmin.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:queastio/models/user.dart';
+import 'package:queastio/shared/constants.dart';
 
 class UserTile extends StatelessWidget {
   final UserData user;
   UserTile({this.user});
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Text(
-          user.name,
-          style: TextStyle(
-            fontSize: 20.0,
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, PrevScoresRoute, arguments: user.uid);
+      },
+      child: Card(
+        child: ListTile(
+          title: Text(
+            user.name,
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
           ),
+          leading: CircleAvatar(backgroundImage: NetworkImage(user.image)),
         ),
-        leading: CircleAvatar(backgroundImage: NetworkImage(user.image)),
       ),
     );
   }
