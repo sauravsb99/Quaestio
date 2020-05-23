bd50863182a219a26c7f79aeccb747fcb0277b6a
sachin-in1 <saching1998@cet.ac.in>
2020-05-23 20:57:56 +0530

    temp

--- queastio/lib/screens/home/topic_list.dart
+++ queastio/lib/screens/home/topic_list.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:queastio/models/topic.dart';
+import 'package:queastio/models/user.dart';
 import 'package:queastio/screens/home/topic_tile.dart';
+import 'package:queastio/services/database.dart';
 
 class TopicList extends StatefulWidget {
+  final UserData user;
+  TopicList({this.user});
+
   @override
   _TopicListState createState() => _TopicListState();
 }
 
 class _TopicListState extends State<TopicList> {
+  final UserData user;
+  String batch ;
+  _TopicListState({this.user});
   @override
   Widget build(BuildContext context) {
-    final topics = Provider.of<List<Topic>>(context);
+    String batch='12';
+    final user = Provider.of<List<User>>(context);
+//    print(userData.batch);
+
+    var uid;
+    return StreamBuilder<List<Topic>>(
+        stream: DatabaseService().getTopics(batch),
+    // ignore: missing_return
+    builder: (context, snapshot) {
+    if (snapshot.hasData) {
+    List<Topic> topics = snapshot.data;
+//    final topics = Provider.of<List<Topic>>(context);
     // topics.forEach((topic) {
     //   print(topic.name);
     //   print(topic.image);
@@ -24,4 +43,12 @@ class _TopicListState extends State<TopicList> {
       },
     );
   }
+    else
+      {
+        return Container();
+      }
+
+});
+  }
 }
+
