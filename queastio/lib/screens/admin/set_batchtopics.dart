e2b98174a8254d82ea3ae137257a34f452db452f
adarsh-av13 <dra4474@gmail.com>
2020-05-24 17:58:10 +0530

    setbatchtopics complete

---
+++ queastio/lib/screens/admin/set_batchtopics.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../../models/batch.dart';
+import '../../models/topic.dart';
+import '../../services/database.dart';
+import '../../shared/constants.dart';
+import '../../shared/constants.dart';
+
+class SetBatchTopics extends StatefulWidget {
+  final Batch batch;
+  final List topics;
+  SetBatchTopics({this.batch, this.topics});
+  @override
+  _SetBatchTopicsState createState() => _SetBatchTopicsState();
+}
+
+class _SetBatchTopicsState extends State<SetBatchTopics> {
+  List<bool> checkboxes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    widget.topics.forEach((topic) {
+      if (widget.batch.topics.contains(topic.name))
+        checkboxes.add(true);
+      else
+        checkboxes.add(false);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Topics'), actions: <Widget>[
+          FlatButton(
+            onPressed: () async {
+              List<String> newTopics = [];
+              for (var i = 0; i < widget.topics.length; ++i) {
+                if (checkboxes[i] == true) {
+                  newTopics.add(widget.topics[i].name);
+                }
+              }
+              Batch newBatch = Batch(
+                  bid: widget.batch.bid,
+                  name: widget.batch.name,
+                  topics: newTopics);
+              await DatabaseService().updateBatchTopics(newBatch);
+              Navigator.pop(context, {'batch': newBatch});
+            },
+            child: Text('Save', style: buttonText),
+          )
+        ]),
+        body: ListView.builder(
+            itemCount: widget.topics.length,
+            itemBuilder: (context, index) {
+              return CheckboxListTile(
+                  value: checkboxes[index],
+                  onChanged: (value) {
+                    setState(() {
+                      checkboxes[index] = value;
+                    });
+                  },
+                  title: Text(widget.topics[index].name));
+            }));
+  }
+}
